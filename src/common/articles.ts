47aa1152a38434ed3eb2c2924b17def9ce1696b6
leospiders <leospiders@localhost.localdomain>
2025-09-12 13:28:32 -0400

    workflow

---
+++ src/common/articles.ts
@@ -0,0 +1,55 @@
+interface New {
+  title: string;
+  slug: string;
+  description: string;
+  content: string;
+}
+export const articles: New[] = [
+  {
+    title: "Hydrogen VS Electric Cars",
+    slug: "hydrogen-vs-electric-cars",
+    description: "Will hydrogen-fueled cars ever catch up to EVs?",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo, corporis nobis ipsa maiores praesentium? Modi, quidem soluta odio placeat nobis, ducimus rem voluptates sit quod, ipsa corporis voluptatem dolorum facere distinctio cumque voluptatibus fuga consequatur totam voluptas expedita odit blanditiis quaerat? Optio nihil possimus animi sed unde voluptate aut.",
+  },
+  {
+    title: "The Downsides of AI Artistry",
+    slug: "the-downsides-of.ia-artistry",
+    description:
+      "What are the possible adverse effects of on-demand AI image generation?",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo, corporis nobis ipsa maiores praesentium? Modi, quidem soluta odio placeat nobis, ducimus rem voluptates sit quod, ipsa corporis voluptatem dolorum facere distinctio cumque voluptatibus fuga consequatur totam voluptas expedita odit blanditiis quaerat? Optio nihil possimus animi sed unde voluptate aut.",
+  },
+  {
+    title: "Is VC Funding Drying Up?",
+    slug: "is-vs-funding-drying-up",
+    description:
+      "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo, corporis nobis ipsa maiores praesentium? Modi, quidem soluta odio placeat nobis, ducimus rem voluptates sit quod, ipsa corporis voluptatem dolorum facere distinctio cumque voluptatibus fuga consequatur totam voluptas expedita odit blanditiis quaerat? Optio nihil possimus animi sed unde voluptate aut.",
+  },
+  {
+    title: "Course: Is VC Funding",
+    slug: "course-funding-drying-up",
+    description:
+      "This is a improved new course VC firms is down 50% YOY. We take a look at what that means.",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo, corporis nobis ipsa maiores praesentium? Modi, quidem soluta odio placeat nobis, ducimus rem voluptates sit quod, ipsa corporis voluptatem dolorum facere distinctio cumque voluptatibus fuga consequatur totam voluptas expedita odit blanditiis quaerat? Optio nihil possimus animi sed unde voluptate aut.",
+  },
+  {
+    title: "Bootcamp: Is VC ",
+    slug: "bootcamp-funding-drying-up",
+    description:
+      "This is a improved new course VC firms is down 50% YOY. We take a look at what that means.",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo, corporis nobis ipsa maiores praesentium? Modi, quidem soluta odio placeat nobis, ducimus rem voluptates sit quod, ipsa corporis voluptatem dolorum facere distinctio cumque voluptatibus fuga consequatur totam voluptas expedita odit blanditiis quaerat? Optio nihil possimus animi sed unde voluptate aut.",
+  },
+  {
+    title: "Talk about: Is  Funding",
+    slug: "talk-funding-drying-up",
+    description:
+      "This is a improved new course VC firms is down 50% YOY. We take a look at what that means.",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A nemo, corporis nobis ipsa maiores praesentium? Modi, quidem soluta odio placeat nobis, ducimus rem voluptates sit quod, ipsa corporis voluptatem dolorum facere distinctio cumque voluptatibus fuga consequatur totam voluptas expedita odit blanditiis quaerat? Optio nihil possimus animi sed unde voluptate aut.",
+  },
+];
